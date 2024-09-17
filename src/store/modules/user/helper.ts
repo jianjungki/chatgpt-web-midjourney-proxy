d306c4ff0ae886a1d398e241b2fdd72f03c79672
GuoRuqiang <61670021+guoruqiang@users.noreply.github.com>
2024-09-17 19:06:02 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -18,7 +18,7 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://www.nwafu-ai.me/images/pngs/mjchat.png',
+      avatar: 'https://main.nwafu-ai.cn/images/pngs/mjchat.png',
       name:  t('mjset.sysname'),//'西农er’s GPT',
       // backgroundImage: 'https://www.nwafu-ai.me/images/jpg/background.jpg', // 默认图片
       description: '专为<a href="https://gpt.nwafu-ai.me" class="text-blue-500" target="_blank" >西农er</a>服务',
